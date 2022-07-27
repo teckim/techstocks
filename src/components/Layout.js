43497a6583e0ff5384508c26045dfd55e4638b48
BHDHakim <hakim.bhd@gmail.com>
2022-07-27 11:46:50 +0300

    Update layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,11 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Appbar from './Appbar';
 
 const Layout = () => (
-  <main>
-    <div className="w-full max-w-md mx-auto bg-pink-500 text-white">
-      <Appbar />
-      <div>
-        <Outlet />
+  <main className="text-white">
+    <div className="mx-auto my-8 rounded-3xl overflow-hidden py-10 shadow-2xl bg-slate-200 relative" style={{ width: '390px', height: '750px' }}>
+      <div className="absolute inset-x-0 z-10">
+        <Appbar />
+      </div>
+      <div className="w-full h-full pt-14 overflow-y-auto bg-pink-500">
+        <div>
+          <Outlet />
+        </div>
       </div>
     </div>
   </main>
